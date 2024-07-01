b4f4c530b3d2c0af5819301082919d7c517990bd
TalalAbdullah595 <talalabdullah108@gmail.com>
2024-07-01 17:55:35 +0500

    Included Packages

---
+++ lib/pages/WeatherPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Weatherpage extends StatelessWidget {
+  const Weatherpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
