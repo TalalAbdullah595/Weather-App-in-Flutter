2e50a38aceec0702c4aae08bbd1d5eb7c148cb76
TalalAbdullah595 <talalabdullah108@gmail.com>
2024-07-02 18:10:31 +0500

    Modified WeatherPage

--- lib/pages/WeatherPage.dart
+++ lib/pages/WeatherPage.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/Api_key.dart';
+import 'package:weatherapp/models/Weathermodel.dart';
+import 'package:weatherapp/services/Weatherservice.dart';
 
-class Weatherpage extends StatelessWidget {
+class Weatherpage extends StatefulWidget {
   const Weatherpage({super.key});
 
   @override
+  State<Weatherpage> createState() => _WeatherpageState();
+}
+
+class _WeatherpageState extends State<Weatherpage> {
+
+  //api key
+  final _weatherService = Weatherservice(API_KEY);
+  //weather object
+  Weather? weather;
+
+  //fetch weather method
+  fetchWeather() async{
+
+  //fetch city name  
+  final cityName = await _weatherService.getCurrentCity();
+
+  try{
+    _weatherService.getWeather(cityName);
+  }
+  catch(e){
+    print(e);
+  }
+  }
+
+
+
+
+  @override
+  @override
+  void initState() {
+    super.initState();
+
+    fetchWeather();
+  }
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(weather?.cityName ?? "Loading City..."),
+
+          Text("${weather?.temperature.round()}C")
+        ],
+      ),
+    );
   }
 }
